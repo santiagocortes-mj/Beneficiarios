6ad5e8f21e181a996afceac95eb12b87b5dbc3f7
santiagocortes-mj <santiagocmj79@hotmail.com>
2021-06-10 19:05:31 -0500

    Código para mapeo.

--- Mapeo.R
+++ Mapeo.R
@@ -3,8 +3,6 @@ library(readr)
 
 # Data ====
 
-View(funcionarios_21)
-
 padron_2014 <- read.csv("https://www.dropbox.com/s/522uhqpgnbdmtl3/padron_2014.csv?dl=1")
 
 padron_2015 <- read.csv("https://www.dropbox.com/s/rzj3rupzd29tkuh/padron_2015.csv?dl=1")
@@ -17,3 +15,15 @@ padron_2018 <- read.csv("https://www.dropbox.com/s/47hc9lzbhto3ypi/padron_2018.c
 
 padron_2019 <- read.csv("https://www.dropbox.com/s/pjq29bl5gbeo2wm/padron_2019.csv?dl=1")
 
+# Collapse ====
+
+View(funcionarios_21) # no podemos crear un CSV hasta tener la base completa
+
+funcionarios_21_2 <- funcionarios_21 %>% 
+    group_by(apellido_paterno, apellido_materno) %>% 
+    summarise(n = n())
+
+# Merge ====
+
+ejemplo_1 <- left_join(padron_2019, funcionarios_21_2, 
+                       by = c("apellido_paterno", "apellido_materno"))
